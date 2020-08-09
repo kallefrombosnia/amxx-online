f07d2b07ade15394b88a4e5361d5171765ea053e
kallefrombosnia <kallegowild@gmail.com>
2020-08-10 00:14:21 +0200

    api: exposing routes  and working on compile

--- src/routes/public/apiRouter.js
+++ src/routes/public/apiRouter.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const AMXX_Object = require('../../amxx/amxx');
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync');
+
+
+const adapter = new FileSync('db/db.json')
+
+const db = low(adapter);
+
+db.defaults({ compiles: [], total_compile_times: 0, total_compile_time: 0}).write();
+
+const amxx = new AMXX_Object();
 
 // Serve default api response 
 router.get('/', (req, res, next) =>{
@@ -19,9 +31,9 @@ router.get('/info', (req, res, next) =>{
             "total_compile_times": 1000,
             "total_compile_seconds": 100000,
             "amxx_versions_supported": [
-                "1.8.3",
-                "1.1.0",
-                "1.9"
+                "1.8.2",
+                "1.10",
+                "1.9.0"
             ]
         }
     });    
@@ -29,13 +41,63 @@ router.get('/info', (req, res, next) =>{
 
 router.post('/compile', (req, res, next) =>{
 
+    // Get values from body
+    const {version, plugin, includes} = req.body;
+
+    // Check if version is set, if not return bad request status code and error message
+    if(!version){
+        return res.status(400).json({
+            "status_code": 400,
+            "message": "AMXX version must be set in request body",
+        });
+    } 
+
+    // Generate unique id for this compile time 
+    let id = amxx.generateUniqueID();
+
+    // Get current record in compiles
+    const compiles = db.get('compiles').value();
+
+    // Check if record exists, if yes assign new id
+    compiles.forEach(compileRecord => {
+        if(compile && compile.id === id){
+            // If it already exists, assign new id - but this should never happen
+            id = amxx.generateUniqueID();
+        }
+    });
+
+
+    // Save includes
+    if(includes && includes.length > 0){
+
+        let tempIncNameArray = [];
+
+        includes.forEach(include =>{
+
+            //const fileName = amxx.filePathGenerator(id, include.incName);
+
+            const fileName = include.incName;
+            
+            // Proccess .inc file
+            amxx.processInclude(fileName, include.value, version);
+
+            tempIncNameArray.push(fileName);
+            
+
+        })
+    }
+
+    /*
+    db.get('compiles').push({
+        id,
+        includes: tempIncNameArray,
+        plugin: plugin
+    })
+    */
+
+
+
     
-    console.log(req.body)
-    
-    res.json({
-        "status_code": 200,
-        "message": "Its working."
-    });    
 });
 
 module.exports = router;
